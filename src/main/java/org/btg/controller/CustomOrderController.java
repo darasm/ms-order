50b1c6ed0cdf02d627bcf517909656b4aed09763
darasm <dara_souza.12@hotmail.com>
2023-08-21 14:32:48 -0300

    feat: add controller

---
+++ src/main/java/org/btg/controller/CustomOrderController.java
@@ -0,0 +1,11 @@
+package org.btg.controller;
+
+import jakarta.ws.rs.core.Response;
+import org.btg.client.CustomOrderAPI;
+
+public class CustomOrderController implements CustomOrderAPI {
+    @Override
+    public Response getClientOrders(Integer clientId) {
+        return CustomOrderAPI.super.getClientOrders(clientId);
+    }
+}
