23d985668c661c83a43236e65294c206106a84c6
darasm <dara_souza.12@hotmail.com>
2023-08-24 07:40:36 -0300

    feat: send message helper

---
+++ src/main/java/org/btg/client/OrderRequestAPI.java
@@ -0,0 +1,24 @@
+package org.btg.client;
+
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import org.btg.client.dto.OrderRequest;
+import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+
+@Path("/v1/orders")
+@Tag(name = "Produce Orders", description = "Create order requests")
+public interface OrderRequestAPI {
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @APIResponse(responseCode = "201", description = "Order created successfully")
+    @APIResponse(responseCode = "400", description = "Missing some important information")
+    @APIResponse(responseCode = "409", description = "Order already exists")
+    @APIResponse(responseCode = "500", description = "Unable to create order due to an internal error")
+    default void createOrder(@RequestBody OrderRequest order){
+        throw new ServiceUnavailableException("Endpoint not implemented yet");
+    }
+}
