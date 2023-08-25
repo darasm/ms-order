4f70645d99b070a01a91900f773873567a2d1450
darasm <dara_souza.12@hotmail.com>
2023-08-25 05:05:43 -0300

    feat: criando metodo no mapper

--- src/main/java/org/btg/mapper/OrderMapper.java
+++ src/main/java/org/btg/mapper/OrderMapper.java
@@ -3,9 +3,14 @@ package org.btg.mapper;
 import org.btg.client.dto.OrderRequest;
 import org.btg.entity.OrderEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "cdi")
 public interface OrderMapper {
 
+    @Mapping(target = "totalPrice", ignore = true)
+    @Mapping(target = "createAt", ignore = true)
+    @Mapping(target = "updateAt", ignore = true)
+    @Mapping(target = "id", source = "request.orderId")
     OrderEntity toEntity(OrderRequest request);
 }
