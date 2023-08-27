08180fa4fa4ad33a24fdf52fd0988430ed4098cc
darasm <dara_souza.12@hotmail.com>
2023-08-27 19:33:19 -0300

    refact: paginationInfoMapper

--- src/test/java/org/btg/mapper/PaginationMapperTest.java
+++ src/test/java/org/btg/mapper/PaginationMapperTest.java
@@ -4,9 +4,11 @@ import io.quarkus.hibernate.orm.panache.PanacheQuery;
 import io.quarkus.panache.common.Page;
 import io.quarkus.test.junit.QuarkusTest;
 import jakarta.inject.Inject;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @QuarkusTest
 
@@ -18,13 +20,13 @@ class PaginationMapperTest {
     @Test
     void testFromPanacheQueryObj() {
 
-        PanacheQuery query = Mockito.mock(PanacheQuery.class);
+        PanacheQuery query = mock(PanacheQuery.class);
 
-        Mockito.when(query.page()).thenReturn(new Page(2, 5));
+        when(query.page()).thenReturn(new Page(2, 5));
         var response = mapper.from(query);
 
-        Assertions.assertEquals(3, response.getPage());
-        Assertions.assertEquals(5, response.getPageSize());
+        assertEquals(3, response.getPage());
+        assertEquals(5, response.getPageSize());
     }
 
 }
