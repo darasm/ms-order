e43f017fbc763bbec2ddd6df84cd4a33812791c3
darasm <dara_souza.12@hotmail.com>
2023-08-27 10:51:40 -0300

    refact: teste controler

--- src/test/java/org/btg/repository/OrderRepositoryTest.java
+++ src/test/java/org/btg/repository/OrderRepositoryTest.java
@@ -11,7 +11,6 @@ import org.mockito.Mockito;
 
 import java.util.List;
 
-import static org.btg.fixtures.ClientOrderInfoFixture.createClientOrderInfoWithTwoItems;
 import static org.btg.fixtures.ClientOrderInfoFixture.createOrderForTwoClients;
 import static org.btg.fixtures.OrderEntityFixture.createOrderEntityWithOneItem;
 import static org.btg.fixtures.OrderEntityFixture.createOrderEntityWithTwoItems;
@@ -42,7 +41,7 @@ class OrderRepositoryTest {
         var secondItemSecondClient = createOrderEntityWithOneItem(4L).setClientId(2L);
 
         PanacheEntityBase.persist(List.of(orderEntity, secondItem));
-        PanacheEntityBase.persist(List.of(orderEntitySecondClient,secondItemSecondClient));
+        PanacheEntityBase.persist(List.of(orderEntitySecondClient, secondItemSecondClient));
 
 
         var expectedResponse = createOrderForTwoClients();
@@ -57,29 +56,4 @@ class OrderRepositoryTest {
         assertEquals(expectedResponse, response);
 
     }
-
-//    @Test
-    @Transactional
-    void testClientWithMoreThanOnwOrder(){ //FIXME
-
-        var orderEntity = createOrderEntityWithTwoItems(1L);
-
-        var secondOrderEntity = createOrderEntityWithOneItem(2L);
-
-        PanacheEntityBase.persist(orderEntity);
-        PanacheEntityBase.persist(secondOrderEntity);
-
-        var expectedResponse = createClientOrderInfoWithTwoItems();
-
-        when(orderMapper.toOrdersList(Mockito.any()))
-                .thenReturn(List.of(createOrderWithTwoItemsDTO(),
-                        createOrderIdTwoWithOneItemDTO()));
-
-        var response = repository.findClientsOrders();
-
-        assertFalse(response.isEmpty());
-        assertEquals(expectedResponse, response);
-
-    }
-
 }
